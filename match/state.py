3f5603e24af752e8fdd6202f6b8d2e8b9a75f972
Daniel Keogh <37158241+daniel-keogh@users.noreply.github.com>
2020-03-12 12:12:41 +0000

    Add type hints to states.py

--- match/state.py
+++ match/state.py
@@ -7,9 +7,11 @@ EPSILON = None
 class State:
     """
     This class represents a state of a Non-deterministic finite automaton (NFA).
+
+    Every State has 0, 1, or 2 edges from it.
     """
 
-    def __init__(self, label=EPSILON, edges=[]):
+    def __init__(self, label: str = EPSILON, edges: list = []):
         self.edges = edges
         self.label = label
 
@@ -25,7 +27,7 @@ class Fragment:
     an accept state.
     """
 
-    def __init__(self, start, accept):
+    def __init__(self, start: State, accept: State):
         self.start = start
         self.accept = accept
 
