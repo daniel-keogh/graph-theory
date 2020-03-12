71c3ff89682887759a361a147e2c3c05d0219f9c
Daniel Keogh <37158241+daniel-keogh@users.noreply.github.com>
2020-03-12 12:38:06 +0000

    Add some exception handling

--- match/__main__.py
+++ match/__main__.py
@@ -1,14 +1,22 @@
-#!/usr/bin/env python3
+import sys
 
 from .cli import parse
-from .regex import match
+from .regex import (
+    match,
+    InvalidRegexpError
+)
 
 
 def main():
     args = parse()
 
-    is_match = match(args.regexp, args.text)
-    print(is_match)
+    try:
+        is_match = match(args.regexp, args.text)
+        print(is_match)
+    except InvalidRegexpError as err:
+        print(f"[Error]: {err}", file=sys.stderr)
+    except Exception as err:
+        print(f"[Error]: {type(err).__name__} - {err}", file=sys.stderr)
 
 
 if __name__ == "__main__":
--- match/regex.py
+++ match/regex.py
@@ -1,17 +1,38 @@
-from .state import Fragment, State
 from .shunting import shunting
+from .state import (
+    EPSILON,
+    Fragment,
+    State
+)
 
 
-def compile(infix):
-    postfix = shunting(infix)
-    postfix = list(postfix)[::-1]
+class InvalidRegexpError(Exception):
+    """
+    Raised to indicate a string is not a valid regular expression, and is
+    therefore unable to be compiled into a NFA.
+    """
+    pass
+
+
+def compile_regex(infix: str) -> Fragment:
+    """
+    Compiles a given regular expression `infix` to a NFA Fragment
+    and returns it.
+    """
+
+    postfix: str
+    try:
+        postfix = shunting(infix)
+        postfix = list(postfix)[::-1]
+    except ValueError:
+        raise InvalidRegexpError("Invalid regular expression")
 
     nfa_stack = []
-    newfrag = None
+    newfrag: Fragment = None
 
     while postfix:
-        # Pop a character from postfix
-        c = postfix.pop()
+        c = postfix.pop()       # Pop a character from postfix
+
         if c == '.':
             # Pop two fragments off the stack
             frag1 = nfa_stack.pop()
@@ -64,28 +85,28 @@ def compile(infix):
 
 
 # Add a state to a set and follow all of the e(psilon) arrows
-def follow_es(state, current):
+def follow_es(state: State, current: set):
     # Only do something when we haven't already seen the state
     if state not in current:
         # Put the state itself into current
         current.add(state)
 
         # See whether state is labelled by e(psilon)
-        if state.label is None:
+        if state.label is EPSILON:
             # Loop through the states pointed to by this state
             for x in state.edges:
                 # Follow all of their e(psilons) too
                 follow_es(x, current)
 
 
-def match(regexp, s):
+def match(regexp: str, s: str) -> bool:
     """
-    This function will return `True` if the regular expression `regexp` fully matches the string `s`,
+    This function will return `True` if the regular expression `regexp` (fully) matches the string `s`,
     and `False` otherwise.
     """
 
     # Compile the regular expression into an NFA and ask the NFA if it matches the string s.
-    nfa = compile(regexp)
+    nfa = compile_regex(regexp)
 
     # Try to match the regular expression to the string s
 
@@ -106,7 +127,7 @@ def match(regexp, s):
         # Loop through the previous states
         for state in previous:
             # Only follow arrows not labeled by e(psilon)
-            if state.label is not None:
+            if state.label is not EPSILON:
                 # If the label equals the character we've read
                 if state.label == c:
                     # Add the state at the end of the arrow to current
